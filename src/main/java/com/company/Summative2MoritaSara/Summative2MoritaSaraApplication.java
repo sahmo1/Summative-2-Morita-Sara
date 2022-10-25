5292995c95eef8a94fb6afcb406ed28136467cc0
Sara Morita <sara.morita97@gmail.com>
2022-10-25 14:49:02 -0700

    first commit

---
+++ src/main/java/com/company/Summative2MoritaSara/Summative2MoritaSaraApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative2MoritaSara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative2MoritaSaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative2MoritaSaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative2MoritaSara/Summative2MoritaSaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative2MoritaSara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative2MoritaSaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
